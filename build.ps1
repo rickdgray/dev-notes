d0e6e7cee74fbb6699949dc3e87b6579dae23688
Rick Gray <rickdgray@outlook.com>
2023-05-25 22:49:42 -0500

    typescript building

--- build.ps1
+++ build.ps1
@@ -1,1 +1,2 @@
-sass --style=compressed --no-source-map src/scss/chroma.scss:static/css/chroma.min.css src/scss/theme.scss:static/css/theme.min.css
+sass --style=compressed --no-source-map src\scss\chroma.scss:static\chroma.css src\scss\theme.scss:static\theme.css
+tsc .\src\ts\main.ts --target es2022 --outfile .\static\main.js
---
+++ static/main.js
@@ -0,0 +1,32 @@
+// document.querySelectorAll('.has-sub-menu > a span.mark').forEach(e => {
+// 	e.addEventListener('click', (e) => {
+// 		$( this ).parent().siblings( 'ul' ).slideToggle( 'fast', 'swing', function() {
+// 			var text = '';
+// 			if ( $( this ).is(':visible') ) {
+// 				text = '-';
+// 			}
+// 			else {
+// 				text = '+';
+// 			}
+// 			$( this ).siblings('a').children( 'span.mark' ).text( text );
+// 		});
+// 		e.preventDefault();
+// 	});
+// });
+const main = document.querySelector('main');
+if (main) {
+    document.querySelectorAll('h1, h2, h3, h4, h5, h6')
+        .forEach((element) => {
+        if (element.id) {
+            const headerlink = document.createElement('a');
+            headerlink.setAttribute('class', 'headerlink');
+            headerlink.setAttribute('href', '#' + element.id);
+            headerlink.setAttribute('title', 'Permalink');
+            const icon = document.createElement('i');
+            icon.setAttribute('class', 'fas fa-hashtag');
+            icon.setAttribute('aria-hidden', 'true');
+            headerlink.append(icon);
+            element.append(headerlink);
+        }
+    });
+}
