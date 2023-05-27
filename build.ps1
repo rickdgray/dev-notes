a40ddbed5d5b5bc15f5ac951dd1844417450a46b
Rick Gray <rickdgray@outlook.com>
2023-05-27 18:59:11 -0500

    fix sidebar and better indentation

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
+git submodule update --remote
 sass --style=compressed --no-source-map src\scss\chroma.scss:static\chroma.css src\scss\theme.scss:static\theme.css
 tsc .\src\ts\main.ts --target es2022 --outfile .\static\main.js
