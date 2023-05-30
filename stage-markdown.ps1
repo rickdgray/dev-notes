9d820072950bc7f30de6c2b1e9ef17501be4d7a8
Rick Gray <rickdgray@outlook.com>
2023-05-30 14:18:16 -0500

    remove work stuff

--- stage-markdown.ps1
+++ stage-markdown.ps1
@@ -4,5 +4,7 @@ git submodule update --init --remote
 Copy-Item .\notes\* .\content\ -Recurse -Container -Force
 Remove-Item -r .\content\.obsidian
 Remove-Item -r .\content\Templates
+Remove-Item -r .\content\Work
+Remove-Item .\content\.git -Force
 Remove-Item .\content\.gitignore
 Remove-Item .\content\LICENSE
